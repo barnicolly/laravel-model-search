231695d0164ee2b3fb63f8f0a33f41d0f08aa379
MishaRatnikov <mik009@yandex.ru>
2023-06-12 16:24:41 +0300

    feat: Добавлена базовая структура

---
+++ tests/ApplicationTestCase.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Barnicolly\ModelSearch\Tests;
+
+use Barnicolly\ModelSearch\ModelSearchServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class ApplicationTestCase extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ModelSearchServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $database = 'testbench';
+        $app['config']->set('database.default', $database);
+        $app['config']->set("database.connections.{$database}", [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
